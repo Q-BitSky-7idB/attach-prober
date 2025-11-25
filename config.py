b9248ea8ad76de6f6f28f916bd4037ecd60f2d3a
Bohdan Khuan <kurl0x.e.3z.tw0yb@gmail.com>
2025-11-26 00:39:02 +0200

    Added new forms for data input\output

--- config.py
+++ config.py
@@ -20,17 +20,31 @@ URL_ID_SUFIX = 'id='
 
 # --- Керування діями (для io_handler) ---
 ACTIONS = {
-    'c': 'DECREMENT',
-    'с': 'DECREMENT', # Кирилична 'с'
-    'r': 'INCREMENT',
-    'к': 'INCREMENT', # Кирилична 'к'
-    'd': 'DOWNLOAD',
-    'в': 'DOWNLOAD', # Кирилична 'в'
-    's': 'EXIT_SOFT',
-    'ы': 'EXIT_SOFT', # Кирилична 'ы'
-    'і': 'EXIT_SOFT', # Кирилична 'і'
-    'q': 'EXIT_CLIPBOARD',
-    'й': 'EXIT_CLIPBOARD', # Кирилична 'й'
+    'r': 'RETURN',
+    'к': 'RETURN', # Кирилична 'к'
+    'К': 'RETURN', # Кирилична 'к'
+    
+    'd': 'DECREMENT',
+    'в': 'DECREMENT', # Кирилична 'в'
+    'В': 'DECREMENT', # Кирилична 'В'
+    
+    'e': 'INCREMENT',
+    'у': 'INCREMENT', # Кирилична 'у'
+    'У': 'INCREMENT', # Кирилична 'У'
+    
+    'g': 'DOWNLOAD',
+    'п': 'DOWNLOAD', # Кирилична 'п'
+    'П': 'DOWNLOAD', # Кирилична 'П'
+    'c': 'QUICK_EXIT', # It was 'EXIT_SOFT' : q - ('ы'\'і')
+    'с': 'QUICK_EXIT', # Кирилична 'с'
+    'С': 'QUICK_EXIT', # Кирилична 'С'
+    'q': 'EXIT_NORMAL',
+    'й': 'EXIT_NORMAL', # Кирилична 'й'
+    'Й': 'EXIT_NORMAL', # Кирилична 'Й'
     't': 'TRY_BYPASS',
-    'е': 'TRY_BYPASS' # Кирилична 'е'
+    'е': 'TRY_BYPASS', # Кирилична 'е'
+    'Е': 'TRY_BYPASS', # Кирилична 'Е'
+    'o': 'DEBUG_OUTPUT',
+    'щ': 'DEBUG_OUTPUT', # Кирилична 'щ'
+    'Щ': 'DEBUG_OUTPUT'  # Кирилична 'Щ'
 }
--- data_transformer.py
+++ data_transformer.py
@@ -3,8 +3,8 @@ import re
 
 # Модуль для розбору вводу\воводу інформації яку треба обробляти
 
-def parse_url_from_args(args):
-    """Розбирає URL на префікс, ID та суфікс."""
+def parse_url_args(args):
+    """Розбирає URL на префікс, ID та постікс."""
     match = re.search(config.URL_ID_PATTERN, args, re.I)
     if not match:
         raise ValueError("Не вдалося знайти шаблон 'id=[number]' у посиланні.")
@@ -17,10 +17,30 @@ def get_target_url(prefix, current_id, suffix):
 
 def print_history_in_preview(id, status, filename, length):
     if status==200:
-        io_handler.print_message(f"\n{URL_ID_SUFIX}{id} : {length: >11} : {filename}", color='CYANB')
+        io_handler.print_msg(f"\n{URL_ID_SUFIX}{id} : {length: >11} : {filename}", color='CYANB')
     else:
-        io_handler.print_message(f"\n{URL_ID_SUFIX}{id} : Помилка ({status}) або з'єднання/таймаут.", color='REDB')
+        io_handler.print_msg(f"\n{URL_ID_SUFIX}{id} : Помилка ({status}) або з'єднання/таймаут.", color='REDB')
         
 def print_result_in_queue(id, status, filename, length):
-    io_handler.overwrite_message(f"\n{URL_ID_SUFIX}{id} : {length: >11} : {filename}", color='GREENB')
-    
+    io_handler.overwrite_msg(f"{URL_ID_SUFIX}{id} : {length: >11} : {filename}\n", color='GREENB')
+
+def rewrite_output(id, status, filename, length, act):
+    match act:
+        case 'DOWNLOAD':
+            io_handler.print_msg(f"\r\033M{URL_ID_SUFIX}{id} : {length: >11} : {filename} => Downloaded.\n", color='GREEN')
+        case _:
+            if status == 200:
+                io_handler.print_msg(f"\r\033M{URL_ID_SUFIX}{id} : {length: >11} : {filename}\n", color='GREEN')
+            else:
+                io_handler.print_msg(f"\r\033M{URL_ID_SUFIX}{id} : Помилка ({status}) : Файл не отримано!\n", color='RED')
+
+def get_options(mode):
+    match mode:
+        case 'act_normal':
+            return f"Оберіть дію (E/D: продовжити, G: скачати, C/Q: вийти) [ID: {current_id}]: "
+        case 'act_after':
+            return f""
+        case 'act_error':
+            return f"[T/Е] - Обхід, [D/В] - Декремент, [E/К] - Інкремент, [C/Q] - Вийти: "
+        case _:
+            return "PRESS_ANY: "
