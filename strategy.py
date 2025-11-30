e84d11e224ac1841f44311f0f97828c5493bc123
Bohdan Khuan <kurl0x.e.3z.tw0yb@gmail.com>
2025-11-30 17:39:46 +0200

    Maybe «strategy» module?...

---
+++ strategy.py
@@ -0,0 +1,8 @@
+﻿# strategy.py
+
+import config
+import context
+
+def get_options():
+  if CURRENT.Status == 403:
+    return f"[T/Е] - Обхід, [D/В] - Декремент, [E/У] - Інкремент, [C/Q] - Вийти: "
