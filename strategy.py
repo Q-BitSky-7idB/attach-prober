cee3709d3d88bc542b8292e24d9c34e654f4ded6
Bohdan Khuan <kurl0x.e.3z.tw0yb@gmail.com>
2025-12-11 22:22:17 +0200

    Clean some mess; Write some schemas in project file

--- strategy.py
+++
@@ -1,8 +0,0 @@
-﻿# strategy.py
-
-import config
-import context
-
-def get_options():
-  if CURRENT.Status == 403:
-    return f"[T/Е] - Обхід, [D/В] - Декремент, [E/У] - Інкремент, [C/Q] - Вийти: "
